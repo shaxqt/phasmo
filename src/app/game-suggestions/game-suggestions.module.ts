d4034dd65d721a65a0d9de3054a5d86688e41a39
shax <philiploesch@googlemail.com>
2020-10-18 13:38:31 +0200

    add expansions for ghosts, style evidence-select to one row

--- src/app/game-suggestions/game-suggestions.module.ts
+++ src/app/game-suggestions/game-suggestions.module.ts
@@ -1,14 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { GameSuggestionsComponent } from './game-suggestions.component';
-import { GhostSuggestionComponent } from './ghost-suggestions/ghost-suggestion/ghost-suggestion.component';
-import {EvidenceSuggestionsComponent} from './evidence-suggestions/evidence-suggestions.component';
-import {MatButtonModule, MatCardModule} from '@angular/material';
+import { EvidenceSuggestionsComponent } from './evidence-suggestions/evidence-suggestions.component';
+import { MatButtonModule, MatCardModule, MatExpansionModule } from '@angular/material';
 import { GhostSuggestionsComponent } from './ghost-suggestions/ghost-suggestions.component';
 
 @NgModule({
-  declarations: [GameSuggestionsComponent, GhostSuggestionComponent, EvidenceSuggestionsComponent, GhostSuggestionsComponent],
-  imports: [CommonModule, MatCardModule, MatButtonModule],
+  declarations: [GameSuggestionsComponent, EvidenceSuggestionsComponent, GhostSuggestionsComponent],
+  imports: [CommonModule, MatCardModule, MatButtonModule, MatExpansionModule],
   exports: [GameSuggestionsComponent],
 })
-export class GameSuggestionsModule {}
+export class GameSuggestionsModule { }
